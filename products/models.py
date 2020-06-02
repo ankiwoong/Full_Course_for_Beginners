c40fed5f2ba20f875b7f7087beca750cb1632634
AnKiWoong <ankiwoong@gmail.com>
2020-06-03 07:19:19 +0900

    modeling

--- products/models.py
+++ products/models.py
@@ -6,4 +6,5 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(decimal_places=2, max_digits=10000)
     summary = models.TextField(blank=False, null=True)
-    featured = models.BooleanField()        # null=True, default=True
+    # null=True, default=True
+    featured = models.BooleanField(default=False)
