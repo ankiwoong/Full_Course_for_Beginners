a42277759ea48b67d1f716fb64c9ba98b206715a
AnKiWoong <ankiwoong@gmail.com>
2020-06-03 18:27:22 +0900

    POST & GET

--- products/views.py
+++ products/views.py
@@ -4,21 +4,33 @@ from django.views.decorators.csrf import csrf_exempt
 from .forms import ProductForm
 from .models import Product
 
+
 # Create your views here.
-
-
-@csrf_exempt
 def product_create_view(request):
-    form = ProductForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        form = ProductForm()
+    # print(request.GET)
+    # print(request.POST)
+    if request.method == "POST":
+        my_new_title = request.POST.get('title')
+        print(my_new_title)
 
     context = {
-        'form': form
     }
 
     return render(request, "products/product_create.html", context)
+
+
+# @csrf_exempt
+# def product_create_view(request):
+#     form = ProductForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         form = ProductForm()
+
+#     context = {
+#         'form': form
+#     }
+
+#     return render(request, "products/product_create.html", context)
 
 
 def product_detail_view(request):
