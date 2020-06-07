5370be0ccc71b5b31981aa19aee94c021ebbc3ae
Ankiwoong <ankiwoong@gmail.com>
2020-06-08 07:19:31 +0900

    Add

--- trydjango/urls.py
+++ trydjango/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from pages.views import home_view, contact_view, about_view, social_view
 # from products.views import product_detail_view, product_create_view
 # from products.views import render_initial_data
-from products.views import dynamic_lookup_view, product_delete_view
+from products.views import dynamic_lookup_view, product_delete_view, product_list_view
 
 urlpatterns = [
     path('', home_view, name='home'),
@@ -30,7 +30,7 @@ urlpatterns = [
     path('products/', product_detail_view),
     path('create/', product_create_view),
     path('inital/', render_initial_data),
-    path('products/<int:id>', dynamic_lookup_view, name='product'),
+    path('products/<int:id>', dynamic_lookup_view, name='pr oduct'),
     path('products/<int:id>/delete/', product_delete_view, name='product_delete'),
-
+    path('products/', product_list_view, name='product-list'),
 ]
