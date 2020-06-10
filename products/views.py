a9efed0d7da60992f2aa234bcc946ac092a1c35b
Ankiwoong <ankiwoong@gmail.com>
2020-06-10 21:04:27 +0900

    Form Validation Methods

--- products/views.py
+++ products/views.py
@@ -4,18 +4,18 @@ from .forms import ProductForm, RawProductForm
 from .models import Product
 
 # Create your views here.
-def product_create_view(request):
-    my_form = RawProductForm()
-    if request.method == "POST":
-        my_form = RawProductForm(request.POST)
-        if my_form.is_valid():
-            # now the data is good
-            print(my_form.cleaned_data)
-            Product.objects.create(**my_form.cleaned_data)
-        else:
-            print(my_form.errors)
-    context = {"form": my_form}
-    return render(request, "products/product_create.html", context)  # In App Template
+# def product_create_view(request):
+#     my_form = RawProductForm()
+#     if request.method == "POST":
+#         my_form = RawProductForm(request.POST)
+#         if my_form.is_valid():
+#             # now the data is good
+#             print(my_form.cleaned_data)
+#             Product.objects.create(**my_form.cleaned_data)
+#         else:
+#             print(my_form.errors)
+#     context = {"form": my_form}
+#     return render(request, "products/product_create.html", context)  # In App Template
 
 
 # def product_create_view(request):
@@ -30,15 +30,15 @@ def product_create_view(request):
 #     return render(request, "products/product_create.html", context)  # In App Template
 
 
-# def product_create_view(request):
-#     form = ProductForm(request.POST or None)
-#     if form.is_valid():
-#         form.save()
-#         form = ProductForm()  # Initialize form content
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()  # Initialize form content
 
-#     context = {"form": form}
+    context = {"form": form}
 
-#     return render(request, "products/product_create.html", context)  # In App Template
+    return render(request, "products/product_create.html", context)  # In App Template
 
 
 def product_detail_view(request):
