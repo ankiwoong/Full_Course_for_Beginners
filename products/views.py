96ef602c5a18782a6c31345e6f5f0c603df657ac
Ankiwoong <ankiwoong@gmail.com>
2020-06-10 21:22:13 +0900

    Initial Values for Forms

--- products/views.py
+++ products/views.py
@@ -50,3 +50,13 @@ def product_detail_view(request):
     context = {"object": obj}
     # return render(request, "product/detail.html", context)  # Out App Template
     return render(request, "products/product_detail.html", context)  # In App Template
+
+
+def render_inital_data(request):
+    initial_data = {"title": "My this awesome title"}
+    obj = Product.objects.get(id=1)
+    form = ProductForm(request.POST or None, initial=initial_data, instance=obj)
+    if form.is_valid():
+        form.save()
+    context = {"form": form}
+    return render(request, "products/product_create.html", context)
