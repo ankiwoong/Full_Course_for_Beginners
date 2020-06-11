976e36e410ee7123cdef2cf9a7d6f04798b2656a
Ankiwoong <ankiwoong@gmail.com>
2020-06-11 17:52:32 +0900

    Class Based Views - DetailView

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import ArticleListView
+from .views import ArticleListView, ArticleDetailView
 
 app_name = "articles"
 
-urlpatterns = [path("", ArticleListView.as_view(), name="article-list")]
+urlpatterns = [
+    path("", ArticleListView.as_view(), name="article-list"),
+    path("<int:id>/", ArticleDetailView.as_view(), name="article-detail"),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import (
     CreateView,
-    DeleteView,
+    DetailView,
     ListView,
     UpdateView,
     ListView,
@@ -14,3 +14,12 @@ from .models import Article
 class ArticleListView(ListView):
     template_name = "articles/article_list.html"
     queryset = Article.objects.all()  # blog/<modelname>_list.html
+
+
+class ArticleDetailView(DetailView):
+    template_name = "articles/article_detail.html"
+    # queryset = Article.objects.filter()
+
+    def get_object(self):
+        id_ = self.kwargs.get("id")
+        return get_object_or_404(Article, id=id_)
