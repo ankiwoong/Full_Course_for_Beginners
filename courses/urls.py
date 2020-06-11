814dfd1cca81d287837425d2f0575ad12eceab3d
Ankiwoong <ankiwoong@gmail.com>
2020-06-12 08:19:35 +0900

    Raw Create Class Based View

--- courses/urls.py
+++ courses/urls.py
@@ -4,6 +4,7 @@ from .views import (
     # my_fbv,
     CourseView,
     CourseListView,
+    CourseCreateView,
 )
 
 app_name = "courses"
@@ -12,4 +13,5 @@ urlpatterns = [
     path("", CourseListView.as_view(), name="courses-list"),
     # path("", my_fbv, name="courses-list"),
     path("<int:id>/", CourseView.as_view(), name="courses-detail"),
+    path("create/", CourseCreateView.as_view(), name="courses-create"),
 ]
