5f37038290fb0fe03f6ccf9c39196ba5f960cb64
Ankiwoong <ankiwoong@gmail.com>
2020-06-12 07:51:06 +0900

    Raw List Class Based View

--- courses/urls.py
+++ courses/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 
-from .views import my_fbv, CourseView
+from .views import (
+    # my_fbv,
+    CourseView,
+    CourseListView,
+)
 
 app_name = "courses"
 
 urlpatterns = [
-    path("", CourseView.as_view(template_name="contact.html"), name="courses-list"),
+    path("", CourseListView.as_view(), name="courses-list"),
     # path("", my_fbv, name="courses-list"),
     path("<int:id>/", CourseView.as_view(), name="courses-detail"),
 ]
--- courses/views.py
+++ courses/views.py
@@ -4,6 +4,22 @@ from django.views import View
 from .models import Course
 
 # BASE_VIEW Class = VIEW
+class CourseListView(View):
+    template_name = "courses/course_list.html"
+    queryset = Course.objects.all()
+
+    def get_queryset(self):
+        return self.queryset
+
+    def get(self, request, *args, **kwargs):
+        context = {"object_list": self.get_queryset()}
+        return render(request, self.template_name, context)
+
+
+class MyListView(CourseListView):
+    queryset = Course.objects.filter(id=1)
+
+
 class CourseView(View):
     template_name = "courses/course_detail.html"  # DetailView
 
