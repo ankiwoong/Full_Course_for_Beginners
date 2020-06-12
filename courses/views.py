c7195cdb968a65a14f91aa98626c67a0311ae7a3
Ankiwoong <ankiwoong@gmail.com>
2020-06-12 17:53:59 +0900

    Custom Mixin for Class Based Views

--- courses/views.py
+++ courses/views.py
@@ -4,16 +4,22 @@ from django.views import View
 from .forms import CourseModelForm
 from .models import Course
 
-# BASE_VIEW Class = VIEW
-class CourseDeleteView(View):
-    template_name = "courses/course_delete.html"
+# BASE VIEW CLass = VIEW
+
+
+class CourseObjectMixin(object):
+    model = Course
 
     def get_object(self):
         id = self.kwargs.get("id")
         obj = None
         if id is not None:
-            obj = get_object_or_404(Course, id=id)
+            obj = get_object_or_404(self.model, id=id)
         return obj
+
+
+class CourseDeleteView(CourseObjectMixin, View):
+    template_name = "courses/course_delete.html"  # DetailView
 
     def get(self, request, id=None, *args, **kwargs):
         # GET method
@@ -34,8 +40,8 @@ class CourseDeleteView(View):
         return render(request, self.template_name, context)
 
 
-class CourseUpdateView(View):
-    template_name = "courses/course_update.html"
+class CourseUpdateView(CourseObjectMixin, View):
+    template_name = "courses/course_update.html"  # DetailView
 
     def get_object(self):
         id = self.kwargs.get("id")
@@ -68,7 +74,7 @@ class CourseUpdateView(View):
 
 
 class CourseCreateView(View):
-    template_name = "courses/course_create.html"
+    template_name = "courses/course_create.html"  # DetailView
 
     def get(self, request, *args, **kwargs):
         # GET method
@@ -98,26 +104,20 @@ class CourseListView(View):
         return render(request, self.template_name, context)
 
 
-class MyListView(CourseListView):
-    queryset = Course.objects.filter(id=1)
-
-
-class CourseView(View):
+class CourseView(CourseObjectMixin, View):
     template_name = "courses/course_detail.html"  # DetailView
 
     def get(self, request, id=None, *args, **kwargs):
         # GET method
-        context = {}
-        if id is not None:
-            obj = get_object_or_404(Course, id=id)
-            context["object"] = obj
-        return render(request, self.template_name, context)  # new_obj = CourseView()
+        context = {"object": self.get_object()}
+        return render(request, self.template_name, context)
 
     # def post(request, *args, **kwargs):
-    #     return render(request, "about.html", {})
+    #     return render(request, 'about.html', {})
 
 
 # HTTP METHODS
 def my_fbv(request, *args, **kwargs):
-    print(request, method)
+    print(request.method)
     return render(request, "about.html", {})
+
